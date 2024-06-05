3dd9d8465ec9d636fcc287ad135f7e3ef1d7c5e1
warren <warren.strange@gmail.com>
2024-06-05 12:29:05 -0600

    dbus getunit, etc

--- lib/src/sysd.dart
+++ lib/src/sysd.dart
@@ -1,9 +1,32 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:dbus/dbus.dart';
 
 import 'systemd.dart';
 
 final sysdSvc = SystemD();
 
+/// DBus modes:
+///  The mode needs to be one of replace, fail, isolate, ignore-dependencies, ignore-requirements.
+enum Mode {
+  // start the unit and its dependencies, possibly replacing already queued jobs that conflict with this.
+  replace,
+  // start the unit and its dependencies, but will fail if this would change an already queued job
+  fail,
+  // start the unit in question and terminate all units that aren't dependencies of it.
+  // This is invalid for stopUnit
+  isolate,
+
+  /// It is not recommended to make use of the latter two options
+  /// Note - these need to be lower cased with a dash to seperate the names
+  /// ignore-dependencies - start a unit but ignore all its dependencies
+  ignoreDependencies,
+  // start a unit but only ignore the requirement dependencies
+  ignoreRequirements,
+}
+
+/// SystemD Dbus interface
+/// See https://www.freedesktop.org/wiki/Software/systemd/dbus/
+///
 class SystemD {
   final client = DBusClient.system();
   late final OrgFreedesktopDBusPeer dbus;
@@ -30,24 +53,122 @@ class SystemD {
       }
     }
 
-    // need to implement comparable first..
-    // l.sort();
+    l.sort(); // Sort by Service unitName
     return l;
   }
 
-  Future<String> stopUnit(String serviceName) async {
-    var r = await dbus.callStopUnit(serviceName, 'replace');
+  Future<String> getUnit(String serviceName) async {
+    var r = await dbus.callGetUnit(serviceName);
+    print(r);
+    return r.value;
+  }
+
+  Future<String> stopUnit(String serviceName,
+      {Mode mode = Mode.replace}) async {
+    var r = await dbus.callStopUnit(serviceName, mode.name);
+    return r.value;
+  }
+
+  Future<String> startUnit(String serviceName,
+      {Mode mode = Mode.replace}) async {
+    var r = await dbus.callStartUnit(serviceName, mode.name);
     return r.value;
   }
 }
 
-class Service {
-  final String name;
-
-  Service({required this.name});
+class Service implements Comparable<Service> {
+  final String unitName;
+  final String description;
+  final String loadeState;
+  final String active;
+  final String subState;
+  final String objectPath;
+  Service({
+    required this.unitName,
+    required this.description,
+    required this.loadeState,
+    required this.active,
+    required this.subState,
+    required this.objectPath,
+  });
 
   factory Service.fromDbusValues(List<DBusValue> dbus) {
-    var x = dbus.first;
-    return Service(name: x.asString());
+    return Service(
+        unitName: dbus[0].asString(),
+        description: dbus[1].asString(),
+        loadeState: dbus[2].asString(),
+        active: dbus[3].asString(),
+        subState: dbus[4].asString(),
+        objectPath: dbus[5].asString());
+  }
+
+  Service copyWith({
+    String? unitName,
+    String? description,
+    String? loadeState,
+    String? active,
+    String? subState,
+    String? objectPath,
+  }) {
+    return Service(
+      unitName: unitName ?? this.unitName,
+      description: description ?? this.description,
+      loadeState: loadeState ?? this.loadeState,
+      active: active ?? this.active,
+      subState: subState ?? this.subState,
+      objectPath: objectPath ?? this.objectPath,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': unitName,
+      'description': description,
+      'loadeState': loadeState,
+      'active': active,
+      'subState': subState,
+      'objectPath': objectPath,
+    };
+  }
+
+  factory Service.fromMap(Map<String, dynamic> map) {
+    return Service(
+      unitName: map['name'] as String,
+      description: map['description'] as String,
+      loadeState: map['loadeState'] as String,
+      active: map['active'] as String,
+      subState: map['subState'] as String,
+      objectPath: map['objectPath'] as String,
+    );
+  }
+
+  @override
+  bool operator ==(covariant Service other) {
+    if (identical(this, other)) return true;
+
+    return other.unitName == unitName &&
+        other.description == description &&
+        other.loadeState == loadeState &&
+        other.active == active &&
+        other.subState == subState &&
+        other.objectPath == objectPath;
+  }
+
+  @override
+  int get hashCode {
+    return unitName.hashCode ^
+        description.hashCode ^
+        loadeState.hashCode ^
+        active.hashCode ^
+        subState.hashCode ^
+        objectPath.hashCode;
+  }
+
+  @override
+  int compareTo(Service other) => unitName.compareTo(other.unitName);
+
+  @override
+  String toString() {
+    return 'Service(unitName: $unitName, description: $description, loadeState: $loadeState, active: $active, subState: $subState, objectPath: $objectPath)';
   }
 }
--- test/dbus_test.dart
+++ test/dbus_test.dart
@@ -1,11 +1,26 @@
 import 'package:test/test.dart';
 import 'package:linux_proc/linux_proc.dart';
 
+final testSvc = 'demo.service';
 main() {
-  test('Dbus stat', () async {
-    var sd = SystemD();
+  var sd = SystemD();
 
-    var r = await sd.stopUnit('demo.service');
+  test('List Dbus', () async {
+    var l = await sd.getUnits();
+    for (var s in l) {
+      print('${s.unitName} ${s.active} ${s.subState}');
+    }
+  });
+  test('Dbus stat', () async {
+    var r = await sd.stopUnit(testSvc);
+    print(r);
+
+    await Future.delayed(Duration(seconds: 5));
+
+    r = await sd.startUnit(testSvc);
+    print(r);
+
+    r = await sd.getUnit(testSvc);
     print(r);
   });
 }
